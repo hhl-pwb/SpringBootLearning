02530aefb19ec5032bdef22ef1440956cde74938
WeiboPan <1525695648@qq.com>
2024-03-18 22:45:48 +0800

    web整合

--- SpringBoot/spring-boot-3/boot3-01-demo/src/main/java/com/atguigu/boot/MainApplication.java
+++ SpringBoot/spring-boot-3/boot3-01-demo/src/main/java/com/atguigu/boot/MainApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class MainApplication {
     public static void main(String[] args) {
         // java10：局部变量类型的自动推断，只能用在局部变量
-        var ioc = SpringApplication.run(MainApplication.class, args);
+        ConfigurableApplicationContext ioc = SpringApplication.run(MainApplication.class, args);
         // 获取容器中所有组件的名字
 //        String[] names = ioc.getBeanDefinitionNames();
         // 2、遍历 :dispatcherServlet、beanNameViewResolver（用于视图解析）、
