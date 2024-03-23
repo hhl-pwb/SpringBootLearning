cd4ecc0a50557a888337c72687097718ef745b36
WeiboPan <1525695648@qq.com>
2024-03-23 23:40:34 +0800

    1

---
+++ SpringBoot/spring-boot-3/boot3-04-web/src/main/java/om/atguigu/web/config/MyConfig.java
@@ -0,0 +1,42 @@
+package om.atguigu.web.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+//@EnableWebMvc //禁用SpringBoot的默认配置
+@Configuration //这是一个配置类
+public class MyConfig /**implements WebMvcConfigurer**/ {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        // 保留以前的默认配置，可以不用写，也是生效的。
+//        WebMvcConfigurer.super.addResourceHandlers(registry);
+//        // 自己写的信规则
+//        registry.addResourceHandler("/static/**")
+//                .addResourceLocations("classpath:/a/","classpath:/b/")
+//                .setCacheControl(CacheControl.maxAge(1180, TimeUnit.SECONDS));
+//    }
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override//配置静态资源
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                WebMvcConfigurer.super.addResourceHandlers(registry);
+                //        // 自己写的信规则
+                registry.addResourceHandler("/static/**")
+                        .addResourceLocations("classpath:/a/", "classpath:/b/")
+                        .setCacheControl(CacheControl.maxAge(1180, TimeUnit.SECONDS));
+            }
+
+            @Override //配置拦截器
+            public void addInterceptors(InterceptorRegistry registry) {
+                WebMvcConfigurer.super.addInterceptors(registry);
+            }
+        };
+    }
+}
---
+++ SpringBoot/spring-boot-3/boot3-04-web/src/main/java/om/atguigu/web/controller/HelloController.java
@@ -0,0 +1,23 @@
+package om.atguigu.web.controller;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Controller
+public class HelloController {
+//    @GetMapping("/a*/b?/{p1:[a-f]]+}/**")
+    @GetMapping("h")
+    @ResponseBody
+    public String hello(HttpServletRequest request, @PathVariable("p1") String path) {
+        log.info("路径变量p1:{}", path);
+        String uri = request.getRequestURI();
+        return uri;
+    }
+}
