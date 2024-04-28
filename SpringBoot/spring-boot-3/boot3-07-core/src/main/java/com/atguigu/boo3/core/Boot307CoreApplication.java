22a26b3d8159c937e3f469b1b61b90146d750f26
WeiboPan <1525695648@qq.com>
2024-04-29 00:05:28 +0800

    1

---
+++ SpringBoot/spring-boot-3/boot3-07-core/src/main/java/com/atguigu/boo3/core/Boot307CoreApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.boo3.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot307CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot307CoreApplication.class, args);
+    }
+
+}
